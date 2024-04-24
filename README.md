731fe5114941cf60180ea8f79b5e35fa530c2b76
AbrJA <ajaimes@occ.com.mx>
2024-04-24 13:34:31 -0600

    First version

---
+++ README.md
@@ -0,0 +1,3 @@
+# StemmerLite
+
+A fork from [Snowball.jl](https://github.com/JuliaText/Snowball.jl) without [WordTokenizers](https://github.com/JuliaText/WordTokenizers.jl) to avoid errors using [PackageCompiler](https://github.com/JuliaLang/PackageCompiler.jl)
---
+++ test/runtests.jl
@@ -0,0 +1,40 @@
+using Snowball
+using Test
+
+@testset "Snowball.jl" begin
+
+    algs = stemmer_types()
+    @test !isempty(algs)
+    @test length(algs) >= 26
+    @test "english" in algs
+
+    for alg in algs
+        stmr = Stemmer(alg)
+        @test stmr.cptr != C_NULL
+        Snowball.release(stmr)
+    end
+
+    test_cases = Dict{String, Any}(
+        "english" => Dict{AbstractString, AbstractString}(
+            "working" => "work",
+            "worker" => "worker",
+            "aβc" => "aβc",
+            "a∀c" => "a∀c",
+            "fishes" => "fish",
+            "fishing" => "fish",
+            "fish" => "fish",
+            "fisher" => "fisher",
+        )
+    )
+
+    for (alg, test_words) in test_cases
+        stmr = Stemmer(alg)
+        for (n,v) in test_words
+            @test v == stem(stmr, n)
+        end
+    end
+    stmr = Stemmer("english")
+
+    @test stem(stmr, [ "The", "fishers", "are", "fishing", "the", "fishes"]) ==
+        [ "The", "fisher", "are", "fish", "the", "fish"]
+end
